82d92763d0727f965e2b72d043e682f5c365a6ec
Deepak tiwari <atrivedi8988@gmail.com>
2022-09-28 23:24:39 +0530

    leftSidebar Completed

--- bitrix24/src/App.js
+++ bitrix24/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { LeftSidebar } from "./components/LeftSidebar/LeftSidebar";
+import { Box } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box height="100%">
+        <LeftSidebar />
+      </Box>
     </div>
   );
 }
