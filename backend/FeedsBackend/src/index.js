c022efb2440065e1a35a185695bcc1762b79a8fc
Abhishek Pratap Solanki <abhisheksolanki1020@gmail.com>
2022-10-01 11:47:33 +0530

    Day-5

--- backend/FeedsBackend/src/index.js
+++ backend/FeedsBackend/src/index.js
@@ -1,14 +1,16 @@
-const express = require("express")
+const express = require("express");
+const messageRouter = require("../router/message.router");
+const Dbconnect = require("../config/db");
 
-const app = express()
+const app = express();
 app.use(express.json());
-
+app.use("/message", messageRouter);
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hello world!</h2>");
-  });
-  
+  res.send("<h2>Hello world!</h2>");
+});
 
-app.listen(8080, ()=>{
-    console.log("Stared at: http://localhost:8080");
-})
+app.listen(8080, async () => {
+  await Dbconnect();
+  console.log("Stared at: http://localhost:8000");
+});
